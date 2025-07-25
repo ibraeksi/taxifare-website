9e2c52c2d101711f7a8d85e111e64cb95ee02dfa
Ibrahim Eksi <95856789+ibraeksi@users.noreply.github.com>
2025-07-25 11:49:16 +0200

    added submit button

--- app.py
+++ app.py
@@ -1,91 +1,56 @@
 import streamlit as st
-
-'''
-# TaxiFareModel front
-'''
-
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
-
-'''
-## Here we would like to add some controllers in order to ask the user to select the parameters of the ride
-
-1. Let's ask for:
-- date and time
-- pickup longitude
-- pickup latitude
-- dropoff longitude
-- dropoff latitude
-- passenger count
-'''
-
-'''
-## Once we have these, let's call our API in order to retrieve a prediction
-
-See ? No need to load a `model.joblib` file in this app, we do not even need to know anything about Data Science in order to retrieve a prediction...
-
-🤔 How could we call our API ? Off course... The `requests` package 💡
-'''
-
-url = 'https://taxifare-688954311280.europe-west10.run.app/predict'
-# url = 'https://taxifare.lewagon.ai/predict'
-
-if url == 'https://taxifare.lewagon.ai/predict':
-
-    st.markdown('Maybe you want to use your own API for the prediction, not the one provided by Le Wagon...')
-
-'''
-
-2. Let's build a dictionary containing the parameters for our API...
-
-3. Let's call our API using the `requests` package...
-
-4. Let's retrieve the prediction from the **JSON** returned by the API...
-
-## Finally, we can display the prediction to the user
-'''
-
-params = {"pickup_datetime":"2014-07-06 19:18:00", "pickup_longitude":-73.950655,
-          "pickup_latitude":40.783282, "dropoff_longitude":-73.984365,
-          "dropoff_latitude":40.769802, "passenger_count":2}
-
 import requests
 import numpy as np
 import urllib
 import matplotlib.pyplot as plt
 import PIL
-from pathlib import Path
 
-response = requests.get(url, params=params).json()
+'''
+# NYC Taxi Fare Prediction
+'''
 
-st.write("The predicted fare price: ", np.round(response['fare'],2), "$")
+with st.form("predict"):
+    pickup_datetime = st.text_input("Enter Pickup Datetime","2014-07-06 19:18:00")
+    pickup_longitude = st.number_input("Pickup Longitude", format="%.6f", value=-73.950655)
+    pickup_latitude = st.number_input("Pickup Latitude", format="%.6f", value=40.783282)
+    dropoff_longitude = st.number_input("Dropoff Longitude", format="%.6f", value=-73.984365)
+    dropoff_latitude = st.number_input("Dropoff Latitude", format="%.6f", value=40.769802)
+    passenger_count = st.number_input("Passenger Count", value=2)
+    submit = st.form_submit_button("Predict 🚀")
 
+url = 'https://taxifare-688954311280.europe-west10.run.app/predict'
 
-# Let's check NYC bouding boxes
-# Load image of NYC map
-bounding_boxes = (-74.3, -73.7, 40.5, 40.9)
+if submit:
+    params = {"pickup_datetime":pickup_datetime, "pickup_longitude":pickup_longitude,
+            "pickup_latitude":pickup_latitude, "dropoff_longitude":dropoff_longitude,
+            "dropoff_latitude":dropoff_latitude, "passenger_count":passenger_count}
 
-url = 'https://wagon-public-datasets.s3.amazonaws.com/data-science-images/07-ML-OPS/nyc_-74.3_-73.7_40.5_40.9.png'
-nyc_map = np.array(PIL.Image.open(urllib.request.urlopen(url)))
+    response = requests.get(url, params=params).json()
+    fare = response['fare']
 
-def plot_on_map(params, BB, nyc_map, s=10, alpha=0.2):
-    fig, ax = plt.subplots(1, 1, figsize=(16,10))
+    st.success(f"The predicted fare price: **${fare:.2f}**")
 
-    ax.scatter(params["pickup_longitude"], params["pickup_latitude"], zorder=1, alpha=alpha, c='red', s=s)
-    ax.annotate("Pickup", (params["pickup_longitude"], params["pickup_latitude"]), fontsize=12, color='red')
-    ax.scatter(params["dropoff_longitude"], params["dropoff_latitude"], zorder=1, alpha=alpha, c='blue', s=s)
-    ax.annotate("Dropoff", (params["dropoff_longitude"], params["dropoff_latitude"]), fontsize=12, color='blue')
-    ax.set_xlim((BB[0], BB[1]))
-    ax.set_ylim((BB[2], BB[3]))
-    ax.set_title('Pickup & Dropoff Locations')
-    ax.imshow(nyc_map, zorder=0, extent=BB)
+    # Load image of NYC map
+    bounding_boxes = (-74.3, -73.7, 40.5, 40.9)
 
-    return fig
+    url = 'https://wagon-public-datasets.s3.amazonaws.com/data-science-images/07-ML-OPS/nyc_-74.3_-73.7_40.5_40.9.png'
+    nyc_map = np.array(PIL.Image.open(urllib.request.urlopen(url)))
 
-# Plot training data on map
-fig = plot_on_map(params, bounding_boxes, nyc_map, s=20, alpha=1)
+    def plot_on_map(params, BB, nyc_map, s=10, alpha=0.2):
+        fig, ax = plt.subplots(1, 1, figsize=(16,10))
 
-st.pyplot(fig)
+        ax.scatter(params["pickup_longitude"], params["pickup_latitude"], zorder=1, alpha=alpha, c='red', s=s)
+        ax.annotate("Pickup", (params["pickup_longitude"], params["pickup_latitude"]), fontsize=12, color='red')
+        ax.scatter(params["dropoff_longitude"], params["dropoff_latitude"], zorder=1, alpha=alpha, c='blue', s=s)
+        ax.annotate("Dropoff", (params["dropoff_longitude"], params["dropoff_latitude"]), fontsize=12, color='blue')
+        ax.set_xlim((BB[0], BB[1]))
+        ax.set_ylim((BB[2], BB[3]))
+        ax.set_title('Pickup & Dropoff Locations')
+        ax.imshow(nyc_map, zorder=0, extent=BB)
+
+        return fig
+
+    # Plot training data on map
+    fig = plot_on_map(params, bounding_boxes, nyc_map, s=20, alpha=1)
+
+    st.pyplot(fig)
